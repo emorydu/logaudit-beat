981a4218ddfce84fe3d386f14505f870d3f05b08
Emory.Du <iceorangeduxiaocheng@gmail.com>
2024-10-28 16:01:39 +0800

    winbeatcli system utils

---
+++ internal/winbeatcli/systemutil/systemutil.go
@@ -0,0 +1,59 @@
+// Copyright 2024 Emory.Du <orangeduxiaocheng@gmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package systemutil
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"syscall"
+)
+
+// IsProcessExist query whether the process exists, process ID and
+// other information based on the process name.
+func IsProcessExist(app string) (bool, string, int, error) {
+	nid := make(map[string]int)
+	cmd := exec.Command("cmd", "/C", "tasklist")
+	output, _ := cmd.Output()
+	n := strings.Index(string(output), "System")
+	if n == -1 {
+		return false, app, -1, fmt.Errorf("process no find")
+	}
+	data := string(output)[n:]
+	fields := strings.Fields(data)
+	for k, v := range fields {
+		if v == app {
+			nid[app], _ = strconv.Atoi(fields[k+1])
+
+			return true, app, nid[app], nil
+		}
+	}
+
+	return false, app, -1, nil
+}
+
+// Kill kills processes based on name.
+func Kill(app string) error {
+	cmd := exec.Command("taskkill", "/f", "/t", "/im", app)
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+	err := cmd.Start()
+	if err != nil {
+		return err
+	}
+	return cmd.Wait()
+}
+
+func Exec(exe string, args string) error {
+	pwd, _ := os.Getwd()
+	cmd := exec.Command("cmd", "/C", filepath.Join(pwd, exe), "-c", filepath.Join(pwd, args))
+	go func() {
+		_ = cmd.Start()
+	}()
+
+	return nil
+}
---
+++ internal/winbeatcli/systemutil/systemutil_test.go
@@ -0,0 +1,18 @@
+// Copyright 2024 Emory.Du <orangeduxiaocheng@gmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package systemutil
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIsProcessExist(t *testing.T) {
+	fmt.Println(IsProcessExist("filebeat.exe"))
+}
+
+func TestKill(t *testing.T) {
+	fmt.Println(Kill("filebeat"))
+}
