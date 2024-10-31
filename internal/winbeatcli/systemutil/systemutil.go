6f6b0e303fce290eb40af4f15801bcbebb14319a
Emory.Du <iceorangeduxiaocheng@gmail.com>
2024-10-31 11:44:53 +0800

    windowsbeatcli startup

--- internal/winbeatcli/systemutil/systemutil.go
+++ internal/winbeatcli/systemutil/systemutil.go
@@ -72,8 +72,7 @@ func Exec(exe string, args string) error {
 
 	}
 	//cmdExec := fmt.Sprintf(`%s\\fluent-bit\\bin\\%s -c %s\\fluent-bit\\%s`, installPath, exe, installPath, args)
-	cmdExec := fmt.Sprintf(`%s\\%s -c %s%s`, pwd, exe, pwd, args)
-	fmt.Println("CMDEXEC:", cmdExec)
+	cmdExec := fmt.Sprintf(`%s%s -c %s%s`, pwd, exe, pwd, args)
 	cmd := exec.Command("cmd.exe")
 	cmd.SysProcAttr = &syscall.SysProcAttr{CmdLine: fmt.Sprintf(`/c %s`, cmdExec), HideWindow: true}
 	err := cmd.Start()
