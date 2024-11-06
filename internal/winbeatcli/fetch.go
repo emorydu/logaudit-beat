1039d4702e20d2b0e7982efdfe6d4bbe2c3908b4
Emory.Du <iceorangeduxiaocheng@gmail.com>
2024-11-06 11:21:43 +0800

    conv nosupport windows

--- internal/winbeatcli/fetch.go
+++ internal/winbeatcli/fetch.go
@@ -8,12 +8,13 @@ import (
 	"bufio"
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"github.com/emorydu/dbaudit/internal/beatcli/systemutil"
 	"github.com/emorydu/dbaudit/internal/common"
 	"github.com/emorydu/dbaudit/internal/common/client"
-	"github.com/emorydu/dbaudit/internal/common/conv"
 	"github.com/emorydu/dbaudit/internal/common/genproto/auditbeat"
+	"github.com/emorydu/dbaudit/internal/common/utils"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -58,7 +59,7 @@ func (s service) FetchConfigAndOp() {
 		return
 	}
 
-	err = conv.DiffPosition(s.rootPath, resp.Convpath)
+	err = DiffPosition(s.rootPath, resp.Convpath)
 	if err != nil {
 		s.log.Errorf("diff position file error: %v", err)
 		return
@@ -226,4 +227,103 @@ func appendToHosts(item string) error {
 	}
 
 	return nil
+}
+
+func DiffPosition(rootPath string, convpath []string) error {
+	positionFile := filepath.Join(rootPath, "position")
+	m := make(map[string]struct{})
+
+	var err error
+
+	for _, path := range convpath {
+		if !strings.HasSuffix(path, "*") {
+			m[fmt.Sprintf(fmt.Sprintf("%s######%s.utf8######", path, path))] = struct{}{}
+		} else {
+			path = path[:len(path)-len("*")]
+			err = utils.EnsureDir(path + "utf8")
+			if err != nil && !errors.Is(err, utils.ErrAlreadyExists) {
+				return err
+			}
+
+			err = filepath.Walk(path, func(p string, info os.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+				// todo sub-dir files...
+				if !info.IsDir() {
+					fmt.Println(p)
+					pp := filepath.Join(path+"utf8", info.Name()+".utf8")
+					m[fmt.Sprintf(fmt.Sprintf("%s######%s######", p, pp))] = struct{}{}
+				}
+
+				return nil
+			})
+
+		}
+
+	}
+	if err != nil {
+		return err
+	}
+
+	existing := make([]string, 0)
+	pf, err := os.Open(positionFile)
+	if err != nil {
+		return err
+	}
+	scanner := bufio.NewScanner(pf)
+	for scanner.Scan() {
+		existing = append(existing, strings.TrimSpace(scanner.Text()))
+
+	}
+	if err = scanner.Err(); err != nil {
+		return err
+	}
+	pf.Close()
+
+	output, err := os.OpenFile(positionFile, os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer output.Close()
+
+	writer := bufio.NewWriter(output)
+
+	for item := range m {
+		if !contains(existing, item) {
+			_, err = writer.WriteString(fmt.Sprintf("%s%d", item, 0) + "\n")
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	for _, item := range existing {
+		spans := strings.Split(item, "######")
+		if len(spans) != 3 {
+			return fmt.Errorf("position file %s contains invalid syntax", positionFile)
+		}
+		if _, exists := m[fmt.Sprintf("%s######%s######", spans[0], spans[1])]; exists {
+			_, err = writer.WriteString(item + "\n")
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	if err = writer.Flush(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func contains(l []string, item string) bool {
+	for _, v := range l {
+		if strings.HasPrefix(v, item) {
+			return true
+		}
+	}
+
+	return false
 }
